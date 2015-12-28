af782e03a73503f1917b1095a364bb2b97ed0091
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-12-29 00:42:39 +0100

    Refactor to welcome page

--- app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/WelcomePage.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/WelcomePage.java
@@ -18,7 +18,7 @@ public class WelcomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_page);
         getSupportActionBar().hide();
     }
 
