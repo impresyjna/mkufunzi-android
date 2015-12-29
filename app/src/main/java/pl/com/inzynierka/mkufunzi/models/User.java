86b396659a0a67a2275b126128d9badb9e49b999
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-12-29 17:25:50 +0100

    User model

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/User.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/User.java
@@ -2,22 +2,29 @@ package pl.com.inzynierka.mkufunzi.models;
 
 import android.util.Log;
 
-import org.json.JSONArray;
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by impresyjna on 16.12.15.
  */
-public class User {
-    private int id;
-    private String login;
-    private String email;
-    private String name;
-    private String surname;
+@Table(name = "Users")
+public class User extends Model{
+
+    @Column(name = "id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    public int id;
+    @Column(name = "login")
+    public String login;
+    @Column(name = "email")
+    public String email;
+    @Column(name = "name")
+    public String name;
+    @Column(name = "surname")
+    public String surname;
 
     private static final String TAG_ID = "id";
     private static final String TAG_LOGIN = "login";
@@ -26,10 +33,11 @@ public class User {
     private static final String TAG_SURNAME = "surname";
 
     public User() {
-
+        super();
     }
 
     public User(JSONObject json) {
+        super();
         try {
             id = json.getInt(TAG_ID);
             login = json.getString(TAG_LOGIN);
@@ -40,65 +48,5 @@ public class User {
             Log.e("UserConstructor", "JSONErrror");
             e.printStackTrace();
         }
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    /**
-     * This method is used to get array of User objects from server
-     * and deserialize it to ArrayList
-     * @param jsonObject This is the object from server
-     * @return ArrayList of User objects
-     */
-    public List<User> getArrayFromJSON(JSONObject jsonObject) {
-        List<User> users = new ArrayList<>();
-        JSONArray jsonArray = new JSONArray();
-        try {
-            jsonArray = jsonObject.getJSONArray("users");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                users.add(new User(jsonArray.getJSONObject(i)));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return users;
     }
 }
