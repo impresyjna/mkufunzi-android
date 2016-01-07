4d6392f686f060b0d138375f268b38316f5ef348
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-01-07 01:57:38 +0100

    Change to use heroic server

--- app/src/main/java/pl/com/inzynierka/mkufunzi/API/ServerConnector.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/API/ServerConnector.java
@@ -7,14 +7,14 @@ public class ServerConnector {
 
     private static ServerConnector instance;
     private JSONParser jsonParser = new JSONParser();
-    private String HOST_IP = "http://trener-mkufunzi.herokuapp.com";
+    private String HOST_IP = "https://trener-mkufunzi.herokuapp.com";
     private String LOGIN = HOST_IP + "/login_mobile";
     private String REGISTER = HOST_IP + "/register_mobile";
     private String MEASURE_TYPES_INDEX = HOST_IP + "/measure_types_index";
     private String GET_MEASUREMENTS = HOST_IP + "/get_measurements_mobile";
     private String POST_MEASUREMENT = HOST_IP + "/post_measurement_mobile";
 
-    private ServerConnector(){
+    public ServerConnector(){
 
     }
 
