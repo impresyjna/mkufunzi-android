a5e70ef58e4d117f6db09378192b3041c0722805
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-01-22 18:09:48 +0100

    WHBMI javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/WHBMI.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/WHBMI.java
@@ -4,25 +4,43 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by impresyjna on 11.01.16.
+ * Class describing object with last weight measurement, last height measurement and units for them
+ * Also hold actual BMI for user
  */
 public class WHBMI {
 
+    /** Field with last weight value */
     public double weightValue;
+    /** Field with weight unit */
     public String weightUnit;
+    /** Field with last height value */
     public double heightValue;
+    /** Field with height unit */
     public String heightUnit;
+    /** Field with BMI value */
     public double BMIValue;
 
+
+    /** Tag from server with weightValue */
     private static final String TAG_WEIGHT_VALUE = "weight_value";
+    /** Tag from server with weightUnit */
     private static final String TAG_WEIGHT_UNIT = "weight_unit";
+    /** Tag from server with heightValue */
     private static final String TAG_HEIGHT_VALUE = "height_value";
+    /** Tag from server with heightUnit */
     private static final String TAG_HEIGHT_UNIT = "height_unit";
 
+    /**
+     * No parameters constructor
+     */
     public WHBMI() {
 
     }
 
+    /**
+     * Constructor called when object has to be created using data from server.
+     * @param json
+     */
     public WHBMI(JSONObject json) {
         weightValue = 0;
         heightValue = 0;
