58daee94b3506156aa66d977f83a63989928ad58
Asia <impresyjna@Air-Joanna.lan>
2016-01-22 22:32:04 +0100

    EyeColor javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/EyeColor.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/EyeColor.java
@@ -8,26 +8,42 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by impresyjna on 14.01.16.
+ * Class describing eyeColor model compatible with model on server. Extends ActiveAndroid model to make easier to connect to sqlite3 database on
+ * device and Java object.
  */
 @Table(name="eye_colors")
 public class EyeColor extends Model{
 
+    /** Field with id from server */
     @Column(name = "ref_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
     public int id;
+    /** Field with eyeColor name */
     @Column(name = "name")
     public String name;
+    /** Field with string value of eyeColor */
     @Column(name = "color")
     public String color;
 
+    /** Tag from server with id */
     private static String TAG_ID = "id";
+    /** Tag from server with name */
     private static String TAG_NAME = "name";
+    /** Tag from server with color */
     private static String TAG_COLOR = "color";
 
+    /**
+     * No parameters constructor
+     */
     public EyeColor() {
         super();
     }
 
+    /**
+     * Constructor
+     * @param color
+     * @param id
+     * @param name
+     */
     public EyeColor(String color, int id, String name) {
         super();
         this.color = color;
@@ -35,6 +51,11 @@ public class EyeColor extends Model{
         this.name = name;
     }
 
+    /**
+     * Constructor called when object has to be created using data from server. Tags describes which field from json should be
+     * saved in field in Java object
+     * @param json
+     */
     public EyeColor(JSONObject json) {
         super();
         try {
@@ -54,6 +75,10 @@ public class EyeColor extends Model{
         }
     }
 
+    /**
+     * Method necessary for Spinner in ProtegeData
+     * @return name of eyeColor on call EyeColor.toString()
+     */
     public String toString() {
         return name;
     }
