d7df1fe0e98636afd50ebee78ce42c61383c3a37
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-01-22 17:59:30 +0100

    MeasureType javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/MeasureType.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/MeasureType.java
@@ -1,7 +1,5 @@
 package pl.com.inzynierka.mkufunzi.models;
 
-import android.util.Log;
-
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
@@ -10,34 +8,54 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by impresyjna on 01.01.16.
+ * Class describing MeasureType object, compatible with server model. Extends ActiveAndroid Model to make easier connection
+ * between sqlite3 database on device and Java object
  */
 @Table(name="measure_types")
 public class MeasureType extends Model{
+    /** Field with id from server */
     @Column(name = "measure_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
     public int id;
+    /** Field with measureType name */
     @Column(name = "name")
     public String name;
+    /** Field with measureType unit */
     @Column(name = "unit")
     public String unit;
+    /** Field which is true if measureType need extra field for value */
     @Column(name = "extra_field")
     public boolean extraField;
+    /** Field with label for first value */
     @Column(name = "first_label")
     public String firstLabel;
+    /** Field with label for second value */
     @Column(name = "second_label")
     public String secondLabel;
 
+    /** Tag on server with measureType id */
     private static final String TAG_ID = "id";
+    /** Tag on server with measureType name */
     private static final String TAG_NAME = "name";
+    /** Tag on server with measureType unit */
     private static final String TAG_UNIT = "unit";
+    /** Tag on server with value if extraField */
     private static final String TAG_EXTRA_FIELD = "extra_field";
+    /** Tag on server with firstLabel */
     private static final String TAG_FIRST_LABEL = "first_label";
+    /** Tag on server with secondLabel */
     private static final String TAG_SECOND_LABEL = "second_label";
 
+    /**
+     * No parameters constructor
+     */
     public MeasureType(){
         super();
     }
 
+    /**
+     * Constructor called when object is created using data from server. Tags describe fields in json with values in Java object 
+     * @param json
+     */
     public MeasureType(JSONObject json){
         super();
         try {
