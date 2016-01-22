0ad679ca0b6f121e2ab85dfa4cf97cd67ae24bb9
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-01-22 17:19:23 +0100

    Card javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/Card.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/Card.java
@@ -1,7 +1,5 @@
 package pl.com.inzynierka.mkufunzi.models;
 
-import android.util.Log;
-
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
@@ -10,22 +8,35 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by impresyjna on 03.01.16.
+ * Class describing card model from server on device. Extends ActiveAndroid Model to make easier connection between
+ * sqlite3 database on device and Java object
  */
 @Table(name="cards")
 public class Card extends Model {
+    /** Field with data about cardId on server */
     @Column(name = "ref_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
     public int id;
+    /** Field with information about whose card is this(ProtegeId) */
     @Column(name = "protege_id")
     public int protegeId;
 
+    /** Tag on server with id */
     private static final String TAG_ID = "id";
+    /** Tag on server with ProtegeId */
     private static final String TAG_PROTEGE_ID = "protege_id";
 
+    /**
+     * No parameters constructor
+     */
     public Card() {
         super();
     }
 
+    /**
+     * Constructor called when object has to be created using data from server.
+     * Tags describe which field from json should be saved in field in object 
+     * @param json
+     */
     public Card(JSONObject json){
         super();
         try {
