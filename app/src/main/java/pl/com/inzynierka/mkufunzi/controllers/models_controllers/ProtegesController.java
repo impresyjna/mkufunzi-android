1d753f84ae75dc3184bd3439d558bda53e57c7da
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-01-23 02:09:19 +0100

    ProtegesController javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/models_controllers/ProtegesController.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/models_controllers/ProtegesController.java
@@ -3,10 +3,13 @@ package pl.com.inzynierka.mkufunzi.controllers.models_controllers;
 import com.activeandroid.ActiveAndroid;
 
 /**
- * Created by impresyjna on 03.01.16.
+ * Class used to control protege objects in app
  */
 public class ProtegesController {
 
+    /**
+     * Method used to clear all records in proteges table in database on device
+     */
     public void clearProteges(){
         ActiveAndroid.execSQL("delete from proteges");
     }
