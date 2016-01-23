9fb67652e8b79a898f754b49c50224689eb4dc0b
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-01-23 01:47:37 +0100

    CardsController javadoc comments

--- app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/models_controllers/CardsController.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/controllers/models_controllers/CardsController.java
@@ -3,10 +3,13 @@ package pl.com.inzynierka.mkufunzi.controllers.models_controllers;
 import com.activeandroid.ActiveAndroid;
 
 /**
- * Created by impresyjna on 03.01.16.
+ * Class used to controll Card objects
  */
 public class CardsController {
 
+    /**
+     * Method used to clear all records from table cards in sqlite3 database on device 
+     */
     public void clearCards(){
         ActiveAndroid.execSQL("delete from cards");
     }
