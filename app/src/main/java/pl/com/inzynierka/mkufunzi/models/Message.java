84623ebaa2fc74d01ac18749e8b2492c48d2e027
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-01-27 11:54:50 +0100

    Message model modification

--- app/src/main/java/pl/com/inzynierka/mkufunzi/models/Message.java
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/models/Message.java
@@ -3,6 +3,10 @@ package pl.com.inzynierka.mkufunzi.models;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by impresyjna on 27.01.16.
  */
@@ -11,11 +15,13 @@ public class Message {
     public int userSendId;
     public int userReceiveId;
     public String content;
+    public Date sendTime;
 
     private final static String TAG_ID = "id";
     private final static String TAG_USER_SEND_ID = "user_send_id";
     private final static String TAG_USER_RECEIVE_ID = "user_receive_id";
     private final static String TAG_CONTENT = "content";
+    private final static String TAG_SEND_TIME = "created_at";
 
     public Message(){
 
@@ -39,6 +45,14 @@ public class Message {
         }
         try {
             this.content = json.getString(TAG_CONTENT);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        try {
+            this.sendTime = format.parse(json.getString(TAG_SEND_TIME));
+        } catch (ParseException e) {
+            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
