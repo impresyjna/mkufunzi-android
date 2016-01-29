45500e8362c58f4539954dc075661409ee6b40a3
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-01-29 01:19:52 +0100

    GetExcerciseTypesIndexMobile class

---
+++ app/src/main/java/pl/com/inzynierka/mkufunzi/API/excercises/ExcercisesTypesIndexMobile.java
@@ -0,0 +1,86 @@
+package pl.com.inzynierka.mkufunzi.API.excercises;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+import pl.com.inzynierka.mkufunzi.API.ServerConnector;
+import pl.com.inzynierka.mkufunzi.controllers.models_controllers.ExcerciseTypesController;
+import pl.com.inzynierka.mkufunzi.models.ExcerciseType;
+
+/**
+ * Created by impresyjna on 29.01.16.
+ */
+public class ExcercisesTypesIndexMobile extends AsyncTask<String, String, JSONObject> {
+
+    private ServerConnector serverConnector = ServerConnector.getInstance();
+    private ProgressDialog pDialog;
+    private AppCompatActivity activity;
+    private List<ExcerciseType> excerciseTypes;
+
+    public void setActivity(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    public void setExcerciseTypes(List<ExcerciseType> excerciseTypes) {
+        this.excerciseTypes = excerciseTypes;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        pDialog = new ProgressDialog(activity);
+        pDialog.setMessage("Trwa ładowanie. Proszę czekać...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(true);
+        pDialog.show();
+    }
+
+
+    @Override
+    protected JSONObject doInBackground(String... args) {
+        try {
+
+            HashMap<String, String> params = new HashMap<>();
+
+            Log.d("request", "starting");
+
+            JSONObject json = serverConnector.getJsonParser().makeHttpRequest(
+                    serverConnector.getEXERCISES_TYPES_INDEX_MOBILE(), "GET", params);
+
+            if (json != null) {
+                Log.d("JSON result", json.toString());
+                return json;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    protected void onPostExecute(JSONObject json) {
+
+        if (pDialog != null && pDialog.isShowing()) {
+            pDialog.dismiss();
+        }
+        try {
+            if (json.has("excercise_types")) {
+                excerciseTypes = new ExcerciseTypesController().getArrayFromJSON(json);
+                Log.e("ExcerciseTypes", excerciseTypes.get(0).name);
+            } else {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
